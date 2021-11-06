806f43db75b8e9db710743bb33b17e3083b61a0d
Izn18 <adil_a@web.de>
2021-11-06 13:27:37 +0100

    Section 6
    made use of useState for the first time. set a text and change it by clicking on a button with a onClickHandler. Also used two values at ones and a conditional for state changes.

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import { Linking, StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+
+  const [welcome, setWelcome] = useState('Welcome!')
+  const [session, setSession] = useState({number: 6, title: 'states'})
+  const [current, setCurrent] = useState(true)
+
+  const onClickHandler = () => {
+    setWelcome('Welcome back!')
+    setSession({number: 7, title: 'styles'})
+    setCurrent(false)
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>First change ever!</Text>
-      <Button title='Google' onPress={ () => {Linking.openURL('https://www.google.de')}}></Button>
+      <Text style={styles.text}>{welcome}</Text>
+      <Text style={styles.text}>This is section {session.number} and is about {session.title}.</Text>
+      <Text style={styles.text}>This is the {current ? 'current session.' : 'next session.'}</Text>
+      <Button title='Next Section' onPress={onClickHandler}></Button>
     </View>
   );
 }
