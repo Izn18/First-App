900ea784e7da82554037ef4257772e94e13abb40
Izn18 <adil_a@web.de>
2022-01-07 16:01:01 +0100

    Styles & Style Sheet

--- App.js
+++ App.js
@@ -3,34 +3,42 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
 
-  const [increment, setIncrement] = useState(0)
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState('Style Test')
 
   const onClickHandler = () => {
-    setIncrement(increment + 5)
-    setCount(count + 1)
+    setName('Style Test is done!')
   }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>{increment}</Text>
-      <Button title='Add' onPress={onClickHandler}></Button>
-      <Text style={styles.text}>You clicked {count} times.</Text>
+      <Text style={styles.text}>{name}</Text>
+      <View style={styles.button}>
+        <Button title='Add' onPress={onClickHandler}></Button>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: '100%',
+    height: '50%',
     backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
+    borderWidth: 10,
+    borderColor: '#ff0000',
+    borderRadius: 25
   },
   text: {
     color: '#ffffff',
-    fontSize: 20,
+    fontSize: 40,
     fontStyle: 'italic',
     margin: 10,
+    textTransform: 'uppercase'
+  },
+  button: {
+    width: 100,
+    height: 100
   }
 });
