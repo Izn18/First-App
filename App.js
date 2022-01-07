babf66a1942fd825bf71907bde02116ba84d0fa2
Izn18 <adil_a@web.de>
2022-01-07 18:33:44 +0100

    FlatList

--- App.js
+++ App.js
@@ -1,44 +1,49 @@
 import React, {useState} from 'react';
-import { StyleSheet, Text, View, ScrollView, RefreshControl } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, RefreshControl, FlatList } from 'react-native';
 
 export default function App() {
 
   const [Items, setItems] = useState([
-    {key:1, item:'Item 1'},
-    {key:2, item:'Item 2'},
-    {key:3, item:'Item 3'},
-    {key:4, item:'Item 4'},
-    {key:5, item:'Item 5'},
-    {key:6, item:'Item 6'},
-    {key:7, item:'Item 7'},
-    {key:8, item:'Item 8'},
-    {key:9, item:'Item 9'},
-    {key:10, item:'Item 10'},
-    {key:11, item:'Item 11'},
-    {key:12, item:'Item 12'},
-    {key:13, item:'Item 13'}
+    {name:'Item 1'},
+    {name:'Item 2'},
+    {name:'Item 3'},
+    {name:'Item 4'},
+    {name:'Item 5'},
+    {name:'Item 6'},
+    {name:'Item 7'},
+    {name:'Item 8'},
+    {name:'Item 9'},
+    {name:'Item 10'},
+    {name:'Item 11'},
+    {name:'Item 12'},
+    {name:'Item 13'}
   ])
 
   const [Refreshing, setRefreshing] = useState(false)
 
   const onRefresh = () => {
     setRefreshing(true)
-    setItems([...Items, { key: 14, item: 'Item14'}])
+    setItems([...Items, { name: 'Item14'}])
     setRefreshing(false)
   }
 
   return (
-    <ScrollView horizontal={false} style={styles.container} refreshControl={ <RefreshControl refreshing={Refreshing} onRefresh={onRefresh} colors={['#ff0000']}/> }>
-      {
-        Items.map((i) => {
-          return (
-            <View style={styles.item} key={i.key}>
-              <Text style={styles.text}>{i.item}</Text>
-            </View>
-          )
-        })
+    <FlatList
+      keyExtractor={(item, index) => index.toString()}
+      data={Items}
+      renderItem={({item}) => (
+        <View style={styles.item}>
+          <Text style={styles.text}>{item.name}</Text>
+        </View>
+      )}
+      refreshControl={
+        <RefreshControl
+          refreshing={Refreshing}
+          onRefresh={onRefresh}
+          colors={['#ff00ff']}
+        />
       }
-    </ScrollView>
+    />
   );
 }
 
