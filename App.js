647995f69d2b5ad50e38e69f12498d926485117f
Izn18 <adil_a@web.de>
2022-01-07 17:02:52 +0100

    Exercise 2

--- App.js
+++ App.js
@@ -5,15 +5,39 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <View style={styles.view1}>
-        <Text style={styles.text}>1</Text>
+      
+      <View style={styles.boxTop}>
+
+        <View style={styles.view1}>
+          <Text style={styles.text}>1</Text>
+        </View>
+        <View style={styles.view2}>
+          <Text style={styles.text}>2</Text>
+        </View>
+        <View style={styles.view3}>
+          <Text style={styles.text}>3</Text>
+        </View>
+      
       </View>
-      <View style={styles.view2}>
-        <Text style={styles.text}>2</Text>
+
+      <View style={styles.view4}>
+        <Text style={styles.text}>4</Text>
       </View>
-      <View style={styles.view3}>
-        <Text style={styles.text}>3</Text>
+      <View style={styles.view5}>
+        <Text style={styles.text}>5</Text>
       </View>
+
+      <View style={styles.boxBottom}>
+
+        <View style={styles.view6}>
+          <Text style={styles.text}>6</Text>
+        </View>
+        <View style={styles.view7}>
+          <Text style={styles.text}>7</Text>
+        </View>
+     
+      </View>
+    
     </View>
   );
 }
@@ -23,8 +47,6 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#000000',
-    alignItems: 'stretch',
-    justifyContent: 'center',
   },
   view1: {
     flex: 1,
@@ -33,16 +55,45 @@ const styles = StyleSheet.create({
     justifyContent: 'center'
   },
   view2: {
-    flex: 1,
+    flex: 2,
     backgroundColor: '#ff00ff',
     alignItems: 'center',
     justifyContent: 'center'
   },
   view3: {
-    flex: 1,
+    flex: 3,
     backgroundColor: '#ffff00',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  view4: {
+    backgroundColor: '#ff0000',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  view5: {
+    backgroundColor: '#00ff00',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  view6: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  view7: {
+    flex: 1,
+    backgroundColor: '#0f00ff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  boxTop: {
+    flexDirection: 'row',
+  },
+  boxBottom: {
+    flex: 1,
+    flexDirection: 'row'
   },
   text: {
     color: '#ffffff',
