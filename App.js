a2bdadbd8dede4838f268fc6befdfd893a237316
Izn18 <adil_a@web.de>
2022-01-08 19:43:41 +0100

    Modal

--- App.js
+++ App.js
@@ -14,30 +14,50 @@ import {
   TouchableOpacity,
   Pressable,
   Alert,
+  Modal,
 } from "react-native";
 
 export default function App() {
   const [name, setName] = useState("");
   const [submitted, setSubmitted] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
   const onPressHandler = () => {
     if (name.length > 0) {
       setSubmitted(!submitted);
     } else {
-      Alert.alert(
-        "Warning",
-        "You have to type in something!",
-        [
-          { text: "Later", onPress: () => console.warn("Later Pressed!") },
-          { text: "Cancel", onPress: () => console.warn("Cancel Pressed!") },
-          { text: "OK", onPress: () => console.warn("OK Pressed!") },
-        ],
-        { cancelable: true, onDismiss: () => console.warn("Alert dismissed!") }
-      );
+      setShowWarning(true);
     }
   };
 
   return (
     <SafeAreaView style={styles.container}>
+      <Modal
+        visible={showWarning}
+        transparent
+        onRequestClose={() => SetshowWarning(false)}
+        animationType="fade"
+        //hardwareAccelerated -> nur für android
+      >
+        <View style={styles.centered_view}>
+          <View style={styles.warning_modal}>
+            <View style={styles.warning_title}>
+              <Text style={styles.text}>WARNING!</Text>
+            </View>
+            <View style={styles.warning_body}>
+              <Text style={styles.text}>
+                The name must be longer than 0 characters.
+              </Text>
+            </View>
+            <Pressable
+              onPress={() => setShowWarning(false)}
+              style={styles.warning_button}
+              android_ripple={{ color: "#fff" }}
+            >
+              <Text style={styles.text}>OK</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
       <Text style={styles.text}>Please write your name:</Text>
       <TextInput
         style={styles.input}
@@ -75,6 +95,7 @@ const styles = StyleSheet.create({
     paddingTop: Platform.OS === "android" ? 25 : 0,
   },
   text: {
+    textAlign: "center",
     color: "#ffffff",
     fontSize: 20,
     fontStyle: "italic",
@@ -103,5 +124,37 @@ const styles = StyleSheet.create({
     height: 50,
     alignItems: "center",
     justifyContent: "center",
+  },
+  centered_view: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#00000099",
+  },
+  warning_modal: {
+    width: 300,
+    height: 300,
+    backgroundColor: "#ffffff",
+    borderWidth: 1,
+    borderColor: "#000",
+    borderRadius: 20,
+  },
+  warning_title: {
+    height: 50,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#ff0",
+    borderTopRightRadius: 20,
+    borderTopLeftRadius: 20,
+  },
+  warning_body: {
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  warning_button: {
+    backgroundColor: "#00ffff",
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
 });
