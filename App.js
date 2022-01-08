71284794cfb71565bcb204dc0a23aa69bf1800bc
Izn18 <adil_a@web.de>
2022-01-08 15:13:38 +0100

    Prettier

--- App.js
+++ App.js
@@ -1,37 +1,44 @@
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, ScrollView, RefreshControl, FlatList, SectionList } from 'react-native';
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  RefreshControl,
+  FlatList,
+  SectionList,
+} from "react-native";
 
 export default function App() {
-
   const DATA = [
     {
-      title:'Title 1',
-      data: ['Item 1-1', 'Item 1-2', 'Item 1-3']
+      title: "Title 1",
+      data: ["Item 1-1", "Item 1-2", "Item 1-3"],
     },
     {
-      title:'Title 2',
-      data: ['Item 2-1', 'Item 2-2', 'Item 2-3', 'Item 2-4']
+      title: "Title 2",
+      data: ["Item 2-1", "Item 2-2", "Item 2-3", "Item 2-4"],
     },
     {
-      title:'Title 3',
-      data: ['Item 3-1', 'Item 3-2', 'Item 3-3']
+      title: "Title 3",
+      data: ["Item 3-1", "Item 3-2", "Item 3-3"],
     },
     {
-      title:'Title 4',
-      data: ['Item 4-1', 'Item 4-2']
+      title: "Title 4",
+      data: ["Item 4-1", "Item 4-2"],
     },
-  ]
+  ];
 
   return (
     <SectionList
       keyExtractor={(item, index) => index.toString()}
       sections={DATA}
-      renderItem={({item}) => (
+      renderItem={({ item }) => (
         <View>
           <Text style={styles.text}>{item}</Text>
         </View>
       )}
-      renderSectionHeader={({section})=>(
+      renderSectionHeader={({ section }) => (
         <View style={styles.item}>
           <Text style={styles.text}>{section.title}</Text>
         </View>
@@ -43,19 +50,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#000000',
+    flexDirection: "column",
+    backgroundColor: "#000000",
   },
   item: {
     margin: 10,
-    backgroundColor: '#00ff00',
-    alignItems: 'center',
-    justifyContent: 'center'
+    backgroundColor: "#00ff00",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: '#ffffff',
+    color: "#ffffff",
     fontSize: 40,
-    fontStyle: 'italic',
+    fontStyle: "italic",
     margin: 10,
-  }
+  },
 });
