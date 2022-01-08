45f2941c34b6b38bb9e19c00eb4083fbe5855af4
Izn18 <adil_a@web.de>
2022-01-08 15:12:41 +0100

    Prettier

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-
-  const [name, setName] = useState('Style Test')
+  const [name, setName] = useState("Style Test");
 
   const onClickHandler = () => {
-    setName('Style Test is done!')
-  }
+    setName("Style Test is done!");
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>{name}</Text>
       <View style={styles.button}>
-        <Button title='Add' onPress={onClickHandler}></Button>
+        <Button title="Add" onPress={onClickHandler}></Button>
       </View>
     </View>
   );
@@ -21,24 +20,24 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: '50%',
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    width: "100%",
+    height: "50%",
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center",
     borderWidth: 10,
-    borderColor: '#ff0000',
-    borderRadius: 25
+    borderColor: "#ff0000",
+    borderRadius: 25,
   },
   text: {
-    color: '#ffffff',
+    color: "#ffffff",
     fontSize: 40,
-    fontStyle: 'italic',
+    fontStyle: "italic",
     margin: 10,
-    textTransform: 'uppercase'
+    textTransform: "uppercase",
   },
   button: {
     width: 100,
-    height: 100
-  }
+    height: 100,
+  },
 });
