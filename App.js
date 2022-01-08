1194e1c748ceea9d96b370b6979a54fc0df3548e
Izn18 <adil_a@web.de>
2022-01-08 15:12:09 +0100

    Prettier

--- App.js
+++ App.js
@@ -1,24 +1,27 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
-
-  const [welcome, setWelcome] = useState('Welcome!')
-  const [session, setSession] = useState({number: 6, title: 'states'})
-  const [current, setCurrent] = useState(true)
+  const [welcome, setWelcome] = useState("Welcome!");
+  const [session, setSession] = useState({ number: 6, title: "states" });
+  const [current, setCurrent] = useState(true);
 
   const onClickHandler = () => {
-    setWelcome('Welcome back!')
-    setSession({number: 7, title: 'styles'})
-    setCurrent(false)
-  }
+    setWelcome("Welcome back!");
+    setSession({ number: 7, title: "styles" });
+    setCurrent(false);
+  };
 
   return (
     <View style={styles.container}>
       <Text style={styles.text}>{welcome}</Text>
-      <Text style={styles.text}>This is section {session.number} and is about {session.title}.</Text>
-      <Text style={styles.text}>This is the {current ? 'current session.' : 'next session.'}</Text>
-      <Button title='Next Section' onPress={onClickHandler}></Button>
+      <Text style={styles.text}>
+        This is section {session.number} and is about {session.title}.
+      </Text>
+      <Text style={styles.text}>
+        This is the {current ? "current session." : "next session."}
+      </Text>
+      <Button title="Next Section" onPress={onClickHandler}></Button>
     </View>
   );
 }
@@ -26,14 +29,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000000',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000000",
+    alignItems: "center",
+    justifyContent: "center",
   },
   text: {
-    color: '#ffffff',
+    color: "#ffffff",
     fontSize: 20,
-    fontStyle: 'italic',
+    fontStyle: "italic",
     margin: 10,
-  }
+  },
 });
