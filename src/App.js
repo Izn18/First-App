2fcb28663e2f1e55e19bf5d1d3d1e6be8ce0fc25
Izn18 <adil_a@web.de>
2022-01-14 18:17:44 +0100

    Clean

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  RefreshControl,
-  FlatList,
-} from "react-native";
+import { StyleSheet, Text, View, RefreshControl, FlatList } from "react-native";
 
 export default function App() {
   const [Items, setItems] = useState([
