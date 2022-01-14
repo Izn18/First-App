6e8f66694264d78b3dcda6f259950294177bf75f
Izn18 <adil_a@web.de>
2022-01-14 18:18:44 +0100

    Clean

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-import React, { useState } from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  ScrollView,
-  RefreshControl,
-  FlatList,
-  SectionList,
-} from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, SectionList } from "react-native";
 
 export default function App() {
   const DATA = [
